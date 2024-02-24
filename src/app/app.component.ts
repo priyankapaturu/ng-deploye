7e04ea4591f5b674b6be953e64af92efc5c7867f
priyanka <paturupriya@gmail.com>
2024-02-24 15:55:13 +1100

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-deployee';
+  title = 'My Jenkins App in Angular-Priyanka ';
 }
